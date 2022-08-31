97cf7cc56d30c482296f5c9b8d6dd547e035c810
vandersonmgpb <vanderson1022@gmail.com>
2022-08-30 21:49:54 -0300

    treinamento de automatização

---
+++ src/main/java/TesteGoogle.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteGoogle {
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
+	}
+
+}
