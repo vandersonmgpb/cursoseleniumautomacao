bd3b3c1a49b5795e15075051cb739f1ed7bbf714
vandersonmgpb <vanderson1022@gmail.com>
2022-08-31 21:50:07 -0300

    Atualizaões na automação

---
+++ src/main/java/TesteCampoTreinamento.java
@@ -0,0 +1,18 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteCampoTreinamento {
+	
+	@Test
+	public void test() {	
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") +"/src/main/resources/componentes.html");
+		
+//	driver.quit();
+
+	}
+}
--- src/main/java/TesteGoogle.java
+++ src/main/java/TesteGoogle.java
@@ -1,11 +1,21 @@
+import java.awt.Point;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TesteGoogle {
-	public static void main(String[] args) {
+	
+	@Test
+	public void test() {
 		WebDriver driver = new FirefoxDriver();
+//		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.manage().window().maximize();
 		driver.get("http://www.google.com");
-		System.out.println(driver.getTitle());
+		Assert.assertEquals("Google", driver.getTitle());
+		driver.quit();
 	}
 
 }
