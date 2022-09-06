7038449dc1f727e4a940fdd040262c78930b3bb8
vandersonmgpb <vanderson1022@gmail.com>
2022-09-05 21:23:26 -0300

    automação no preenchimento de formularios - janela

--- src/main/java/TesteFrameEJanelas.java
+++ src/main/java/TesteFrameEJanelas.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-public class TesteFrame {
+public class TesteFrameEJanelas {
 	@Test
 	public void deveInteragirComFrames() {
 		WebDriver driver = new FirefoxDriver();
@@ -25,6 +25,20 @@ public class TesteFrame {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(msg);
 		
 		driver.quit();
+	}
+	
+	@Test
+	public void deveInteragirComJanelas() {
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		driver.findElement(By.id("buttonPopUpEasy")).click();
+		driver.switchTo().window("Popup");
+		driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+		driver.close();
+		driver.switchTo().window("");
+		driver.findElement(By.tagName("textarea")).sendKeys("e agora?");
 		
 	}
 }
