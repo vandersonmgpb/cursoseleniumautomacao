f37d1e5555262213a6833f8f8b384befafb8bcb0
vandersonmgpb <vanderson1022@gmail.com>
2022-09-15 20:57:50 -0300

    criando suite de testes

--- src/main/java/TesteCadastro.java
+++ src/main/java/TesteCadastro.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class TesteCadastro {
 	
 	private WebDriver driver;
-	private DSL dsl;
 	private CampoTreinamentoPage page;
 	
 	@Before
@@ -18,7 +17,6 @@ public class TesteCadastro {
 		driver = new FirefoxDriver();
 		driver.manage().window().setSize(new Dimension(1200, 765));
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-		dsl = new DSL(driver);
 		page = new CampoTreinamentoPage(driver);
 	}
 	
@@ -43,48 +41,5 @@ public class TesteCadastro {
 		Assert.assertEquals("Comida: Carne", page.obterComidaCadastro() );
 		Assert.assertEquals("Escolaridade: superior", page.obterEscolaridadeCadastro());
 		Assert.assertEquals("Esportes: Futebol", page.obterEsportesCadastro());
-	}
-	
-	@Test
-	public void deveValidarNomeObrigatorio() {
-		page.cadastrar();
-		Assert.assertEquals("Nome eh obrigatorio", dsl.alertaObterTextoEAceita());
-	}
-	
-	@Test
-	public void deveValidarSobrenomeObrigatorio() {
-		page.setNome("Francisco");
-		page.cadastrar();
-		Assert.assertEquals("Sobrenome eh obrigatorio", dsl.alertaObterTextoEAceita());
-	}
-	
-	@Test
-	public void deveValidarSexoObrigatorio() {
-		page.setNome("Francisco");
-		page.setSobrenome("Araujo");
-		page.cadastrar();
-		Assert.assertEquals("Sexo eh obrigatorio", dsl.alertaObterTextoEAceita());
-	}
-	
-	@Test
-	public void deveValidarComidaVegetariana() {
-		page.setNome("Francisco");
-		page.setSobrenome("Araujo");
-		page.setSexoFeminino();
-		page.setComidaCarne();
-		page.setComidaVegetariano();		
-		page.cadastrar();
-		Assert.assertEquals("Tem certeza que voce eh vegetariano?", dsl.alertaObterTextoEAceita());
-	}
-	
-	@Test
-	public void deveValidarEsportistaIndeciso() {
-		page.setNome("Francisco");
-		page.setSobrenome("Araujo");
-		page.setSexoFeminino();
-		page.setComidaCarne();
-		page.setEsporte("Karate", "O que eh esporte?");
-		page.cadastrar();
-		Assert.assertEquals("Voce faz esporte ou nao?", dsl.alertaObterTextoEAceita());
 	}
 }
