58d9129817513bac28de99d607a3f762dc0c2363
vandersonmgpb <vanderson1022@gmail.com>
2022-09-20 09:30:37 -0300

    Automação - interação dinâmica com tabelas II

--- src/main/java/DSL.java
+++ src/main/java/DSL.java
@@ -177,16 +177,44 @@ public class DSL {
 	
 	/*********Tabela*********/
 	
-	public int clicarBotaoTabela(String colunaBrusca, String coluna, String idTabela) {
+	public void clicarBotaoTabela(String colunaBusca, String valor, String colunaBotao, String idTabela) {
+//	procurar coluna do registro
 	WebElement tabela = driver.findElement(By.xpath("//*[@id='elementosForm:tableUsuarios']"));
-	List<WebElement>colunas = tabela.findElements(By.xpath(".//th"));
-	int idColuna = -1;
-	for(int i = 0; i< colunas.size(); i++) {
-		if(colunas.get(i).getText().equals(colunaBusca)) {
-			idColuna = i;
-			break;
-			
+	int idColuna = obterIndiceColuna(colunaBusca, tabela);
+//	return idColuna;
+	
+//	encontrar a linha do registro
+	int idLinha = obterIndiceLinha(valor, tabela, idColuna);
+	
+//	procurar coluna do botao
+	int idColunaBotao = obterIndiceColuna(colunaBotao, tabela);
+	
+//	clicar no botao da celula encontrada
+	WebElement celula = tabela.findElement(By.xpath(".//tr["+idLinha+"]/td["+idColunaBotao+"]"));
+	celula.findElement(By.xpath("./input")).click();
+}
+
+	private int obterIndiceLinha(String valor, WebElement tabela, int idColuna) {
+		List<WebElement> linhas = tabela.findElements(By.xpath("./tbody/tr/td["+idColuna+"]"));
+		int idLinha = -1;
+		for(int i = 0; i< linhas.size(); i++) {
+			if(linhas.get(i).getText().equals(valor)) {
+				idLinha = i+1;
+				break;
+			}
 		}
+		return idLinha;
 	}
-	return idColuna;
+
+	private int obterIndiceColuna(String coluna, WebElement tabela) {
+		List<WebElement>colunas = tabela.findElements(By.xpath(".//th"));
+		int idColuna = -1;
+		for(int i = 0; i< colunas.size(); i++) {
+			if(colunas.get(i).getText().equals(coluna)) {
+				idColuna = i+1;
+				break;
+			}
+		}
+		return idColuna;
+	}
 }
--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -129,4 +129,9 @@ public class TesteCampoTreinamento {
 		WebElement element = driver.findElement(By.id("elementosForm:nome"));
 		js.executeScript("arguments[0].style.border = arguments[1]", element, "solid 4px red");
 	}
+	
+	@Test
+	public void deveClicarBotaoTabela() {
+		dsl.clicarBotaoTabela("Nome", "Francisco", "Botao", "elementos:form:tableUsuarios");
+	}
 }
