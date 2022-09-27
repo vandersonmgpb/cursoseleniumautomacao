02a1cad6e54cc163219c21fec81645b59a32aa5b
vandersonmgpb <vanderson1022@gmail.com>
2022-09-27 09:40:49 -0300

    Atualização no campo de treinamento - espera Explícita

--- src/main/java/TesteSincronismo.java
+++ src/main/java/TesteSincronismo.java
@@ -3,9 +3,12 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TesteSincronismo {
 	private WebDriver driver;
@@ -39,4 +42,14 @@ public class TesteSincronismo {
 		dsl.escrever("novoCampo", "Deu Certo");
 		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 	}
+	
+	@Test
+	public void deveUtilizarEsperaExplicita() throws InterruptedException{
+		dsl.clicarBotao("buttonDelay");
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("novoCampo")));
+		dsl.escrever("novoCampo", "Deu certo?");
+	}
+	
+	
 }
